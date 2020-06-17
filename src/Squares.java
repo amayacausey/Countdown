76cb1286535e0087f9e3f793a6ea67d41af29189
amayacausey <amayacausey@gmail.com>
2020-06-17 11:27:13 -0700

    first commit

---
+++ src/Squares.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Squares {
+            public static void main(String [] args){
+                Scanner sc=new Scanner(System.in);
+                System.out.println("Number \t\t Square");
+                System.out.println("Please Enter a number:");
+                int userInput=sc.nextInt();
+
+                for(int i=1; i<=userInput; i++){
+                    System.out.println(i+"\t\t"+i*i);
+                }
+                System.out.println("loop ends");
+
+
+
+    }
+}
+
+// i++   i=i+1
